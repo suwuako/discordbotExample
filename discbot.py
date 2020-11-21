9d0d68624d9f6650f36f819510d4aaf4cbddf8c6
megumin00 <43295199+megumin00@users.noreply.github.com>
2020-11-21 23:37:46 +0800

    Update discbot.py

--- discbot.py
+++ discbot.py
@@ -1,28 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  8 14:35:42 2020
-
-@author: User
-"""
 import discord
 import nest_asyncio
 from discord.ext import commands
 
-nest_asyncio.apply()
-bot = commands.Bot(command_prefix=';')
+class dsbot:
+    def __init__(self):
+        @bot.event
+        async def on_ready():
+            print('We have logged in as {0.user}'.format(bot))
 
-@bot.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    def onMsg(self):
+        @bot.event
+        async def on_message(message):
+            pass
+            #this code runs whenever the bot reads/detects a message has been sent
     
-@bot.event
-async def on_message(message):
-    pass
-    #this code runs whenever the bot reads/detects a message has been sent
+    def commands(self):
+        @bot.command
+        async def commandName(message):
+            pass    
     
-@bot.command
-async def commandName(message):
-    pass
-    #this code runs whenever the bot reads ;commandName in chat because of [bot = commands.Bot(command_prefix=';')] and the command name being passed in async def
     
-bot.run('bot token')
+    def run(self):
+        
+        bot.run('token-here')
+
+
+if __name__ == '__main__':
+    nest_asyncio.apply()
+    bot = commands.Bot(command_prefix=';')
+    dsbot = dsbot()
+    dsbot.run()
